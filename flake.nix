667adc84d491fa0670ceacb76468ccdd2500821a
fish <kiokunee@gmail.com>
2026-02-18 15:03:38 +0800

    fix: FIX FILE PATHS

--- flake.nix
+++ flake.nix
@@ -16,11 +16,11 @@
         { config, pkgs, ... }:
         {
           # home.file.".config/nvim".source = ./nvim; # Replaced by nvf
-          home.file.".config/niri".source = ../niri;
-          home.file.".config/waybar".source = ../waybar;
+          home.file.".config/niri".source = ./niri;
+          home.file.".config/waybar".source = ./waybar;
           # home.file.".config/mako".source = ./mako; # Replaced by programs.mako
-          home.file.".zshenv.unix".source = ../.zshenv;
-          home.file.".zshrc.unix".source = ../.zshrc;
+          home.file.".zshenv.unix".source = ./.zshenv;
+          home.file.".zshrc.unix".source = ./.zshrc;
         };
     };
 }
